a14a32db95974547b196e70c292e1f6383e44716
William Miller <71334596+WilliamIMiller@users.noreply.github.com>
2023-01-10 15:31:18 +0000

    tweeks

--- Assets/Scripts/Magnetic/CaptiveCog.cs
+++ Assets/Scripts/Magnetic/CaptiveCog.cs
@@ -7,16 +7,19 @@ public class CaptiveCog : MonoBehaviour , IsMagnetic
     [SerializeField] Transform m_limitLeft;
     [SerializeField] Transform m_limitRight;
 
-    public void isBeingMagnetic(Vector2 pushingPlayerPos, Vector2 forceApplied, PlayerMovment player)
+    public void isBeingMagnetic(Vector2 pushingPlayerPos, Vector2 forceApplied,
+        PlayerMovment player)
     {
         if (forceApplied.y < 0)
         {
             //move left untill stop point
-
+            gameObject.transform.position = Vector3.Lerp(transform.position, m_limitLeft.position, 2);
         }
         else if(forceApplied.y > 0)
         {
             //move right untill stoped
+            gameObject.transform.position = Vector3.Lerp(transform.position, m_limitRight.position, 2);
+
         }
         else
         {
