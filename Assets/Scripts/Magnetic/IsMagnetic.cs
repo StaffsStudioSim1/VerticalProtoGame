67c92470fb16ececf4453fea5926973102224c38
William Miller <71334596+WilliamIMiller@users.noreply.github.com>
2023-01-10 12:22:19 +0000

    Magnetts

---
+++ Assets/Scripts/Magnetic/IsMagnetic.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IsMagnetic
+{
+    void isBeingMagnetic(Vector2 pushingPlayerPos, Vector2 forceApplyed, PlayerMovment player);
+
+
+}
