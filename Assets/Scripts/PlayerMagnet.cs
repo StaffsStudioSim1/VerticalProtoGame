77c5061e704596f04fdad2800e0831f843193b56
WilliamIMiller <71334596+WilliamIMiller@users.noreply.github.com>
2023-01-13 19:56:03 +0000

    stuff

--- Assets/Scripts/PlayerMagnet.cs
+++ Assets/Scripts/PlayerMagnet.cs
@@ -21,18 +21,23 @@ public class PlayerMagnet : MonoBehaviour
     {
         if(m_magnetActive) 
         {
-           
+            List<RaycastHit2D> hitList = new List<RaycastHit2D>();
             RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.right,m_magnetPowerLength);
-            foreach(RaycastHit2D hit in hits)
+            hitList.AddRange(hits);
+            hits = Physics2D.RaycastAll(transform.position, Vector2.right + Vector2.down, m_magnetPowerLength);
+            foreach (RaycastHit2D hit in hits)
             {
-                if(hit.transform.GetComponent<IsMagnetic>() != null)
+                if (!hitList.Contains(hit))
                 {
-                    Debug.Log(hit.transform.name);
-                    hit.transform.GetComponent<IsMagnetic>()?.isBeingMagnetic(transform.position,m_directionOfMagnet,
-                        transform.GetComponent<PlayerMovement>());
-                    hit.transform.GetComponent<IsMagnetic>()?.isBeingMagnetic(transform.position,m_directionOfMagnet,transform.GetComponent<PlayerMovement>());
+                    hitList.Add(hit);  
                 }
             }
+
+               foreach (RaycastHit2D hit in hitList)
+            {
+                hit.transform.GetComponent<IsMagnetic>()?.isBeingMagnetic(transform.position,m_directionOfMagnet,transform.GetComponent<PlayerMovement>());                                  
+            }
+            
         }
     }
 
@@ -54,8 +59,9 @@ public class PlayerMagnet : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Vector2 direction = transform.TransformDirection(Vector2.right) * m_magnetPowerLength;
-
+        Vector2 direction2 = transform.TransformDirection(Vector2.right + new Vector2(0,-0.3f)) * (m_magnetPowerLength / 1.2f);
         Gizmos.DrawRay(transform.position, direction);
+        Gizmos.DrawRay(transform.position, direction2);
     }
 
 }
