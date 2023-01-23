49e651e6e16529d0436174e0d836d56b3418eaa7
William Miller <71334596+WilliamIMiller@users.noreply.github.com>
2023-01-23 16:47:25 +0000

    Platform moving beginings

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    [Header("Platform Control")]
     [SerializeField] PlatformDirection m_moving;
     PlatformDirection m_lastDirection;
     [SerializeField] Vector2 m_positionLeft;
     [SerializeField] Vector2 m_positionRight;
-    [SerializeField] float m_bufer;
+    [SerializeField] float m_speed;
+    Vector3 m_origin;
+    [Tooltip("1 if the positons are in the middle of the tile, 1.5 if bounce at definitve end for a 3 tile width")]
+    [SerializeField] Vector2 m_boundrys;
 
+    [Header("PlayerEntrapment")]
+    [SerializeField] Vector2 m_boxSize;
+    [SerializeField] Vector2 m_boxCenter;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_origin = transform.position;
     }
 
     // Update is called once per frame
@@ -22,11 +29,12 @@ public class MovingPlatform : MonoBehaviour
     {
         if(m_moving != PlatformDirection.None)
         {
+            Vector3 startPos = transform.position; 
             if (m_moving == PlatformDirection.Left)
             {
-                transform.position = Vector2.Lerp(transform.position,(Vector2)transform.position + ( m_positionLeft / m_bufer), Time.deltaTime);
-
-                if (transform.position == (transform.position + (Vector3)(m_positionLeft / m_bufer)))
+                transform.position = Vector2.MoveTowards(transform.position,((Vector2)m_origin + m_positionLeft) + m_boundrys, Time.deltaTime * m_speed);
+                
+                if (transform.position == (m_origin + (Vector3)m_positionLeft) + (Vector3)m_boundrys)
                 {
                     ChangeDirection(PlatformDirection.Right);
                 }
@@ -34,13 +42,30 @@ public class MovingPlatform : MonoBehaviour
             }
             else
             {
-                transform.position = Vector2.Lerp(transform.position,transform.position * (m_positionRight / m_bufer), Time.deltaTime);
-                if (transform.position == transform.position + (Vector3)(m_positionRight / m_bufer))
+                transform.position = Vector2.MoveTowards(transform.position,((Vector2)m_origin + m_positionRight) + -m_boundrys, Time.deltaTime * m_speed);
+                
+                if (transform.position == (m_origin + (Vector3)m_positionRight) + -(Vector3)m_boundrys)
                 {
-                    ChangeDirection(PlatformDirection.Right);
+                    ChangeDirection(PlatformDirection.Left);
                 }
             }
+            Vector3 endPos = transform.position;
+            UpdatePlayer(endPos - startPos);
+        }
+    }
 
+    void UpdatePlayer(Vector3 moveVector)
+    {
+        RaycastHit2D[] hits = Physics2D.BoxCastAll((Vector2)transform.position + m_boxCenter, m_boxSize, 0f,Vector2.one);
+
+        foreach(RaycastHit2D hit in hits)
+        {
+            if(hit.transform.GetComponent<PlayerMovement>() != null)
+            {
+                PlayerMovement player = hit.transform.GetComponent<PlayerMovement>();
+                Vector3 newPos = player.transform.position + moveVector;
+                player.transform.position.Set(newPos.x,newPos.y,newPos.z);
+            }
         }
     }
 
@@ -56,18 +81,40 @@ public class MovingPlatform : MonoBehaviour
 
     public void ChangeDirection(PlatformDirection newDrirction)
     {
+        
        if(m_moving != PlatformDirection.None)
-            m_moving = newDrirction;
-       m_lastDirection = newDrirction;
+            m_lastDirection = newDrirction;
+        m_moving = newDrirction;
+      
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Vector2 directionLeft = transform.TransformDirection(transform.position * (m_positionLeft / m_bufer));
-        Vector2 directionRight = transform.TransformDirection(transform.position * (m_positionRight / m_bufer));
+        Vector2 directionLeft = new Vector2();
+        Vector2 directionRight = new Vector2();
 
-        Gizmos.DrawLine(directionRight,directionLeft);
+        
+        if(!Application.isPlaying)
+        {
+            directionLeft = transform.TransformDirection(transform.position + (Vector3)m_positionLeft);
+            directionRight = transform.TransformDirection(transform.position + (Vector3)m_positionRight);
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            directionLeft = transform.TransformDirection(m_origin + (Vector3)m_positionLeft);
+            directionRight = transform.TransformDirection(m_origin + (Vector3)m_positionRight);
+            Gizmos.color = Color.yellow;
+
+        }
+
+
+        Gizmos.DrawLine(directionRight, directionLeft);
+
+        
+
+        Gizmos.DrawRay(transform.position, m_boxCenter);
+        Gizmos.DrawWireCube(transform.position + (Vector3)(m_boxCenter), m_boxSize);
     }
 }
 
